8de41b3f18c9dc291a23616e22af8ea37733825f
devlee77 <tjdcks0928@naver.com>
2023-08-29 02:12:20 +0900

    주사위게임 해결완료

--- 10103_주사위게임.cpp
+++ 10103_주사위게임.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 int main(void) {
-	cout << "hello";
+	int i = 0;
+	int player1 = 100;
+	int player2 = 100;
+	cin >> i;
+
+	while (i--) {
+		int a, b;
+		cin >> a >> b;
+		if (a > b) { player2 -= a; }
+		else if (a < b) { player1 -= b; }
+	}
+	cout << player1 << endl << player2;
 
 	return 0;
 }
